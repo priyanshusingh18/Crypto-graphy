b257849560c3d35f28eb40fdb0f27e8aee14d9c4
priyanshusingh18 <73636745+priyanshusingh18@users.noreply.github.com>
2021-11-24 02:32:12 +0530

    fix: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,36 @@
-import React, {useState, useEffect, useRef} from 'react';
-import {Button, Menu, Typography, Avatar} from 'antd'
-import {Link} from 'react-router-dom';
-import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@ant-design/icons'
-import icon from '../images/cryptocurrency.png'
+import React, { useState, useEffect, useRef } from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+import icon from "../images/cryptocurrency.png";
 const Navbar = () => {
-    const [activeMenu, setActiveMenu] = useState(false)
-   
-     const [screenSize, setScreenSize] = useState(undefined);
-    
-    
-    
-    const ref = useRef();
+  const [activeMenu, setActiveMenu] = useState(true);
 
-    
-    useEffect(() => {
-        const onBodyClick = (event) => {
-            console.log(ref.current, event.target)
-          if (ref.current?.contains(event.target)) {
-            return;
-          }
-          setActiveMenu(false);
-        };
-        document.body.addEventListener("click", onBodyClick);
-            return () => {
-          document.body.removeEventListener("click", onBodyClick);
-        };
-      }, []);
+  const [screenSize, setScreenSize] = useState(undefined);
 
+  const ref = useRef();
 
-    
-    
-    
-    
-    
-    
-
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      console.log(ref.current?.contains(event.target) && screenSize >= 800);
+      if (ref.current?.contains(event.target) || screenSize >= 800) {
+        return;
+      }
+      if (screenSize <= 800) {
+        setActiveMenu(false);
+      }
+    };
+    document.body.addEventListener("click", onBodyClick);
+    return () => {
+      document.body.removeEventListener("click", onBodyClick);
+    };
+  }, []);
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
 
@@ -52,37 +48,43 @@ const Navbar = () => {
       setActiveMenu(true);
     }
   }, [screenSize]);
-    
-    
-    
-    
 
-
-    return(
-        <div className="nav-container">
-             <div className="logo-container">
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
         <Avatar src={icon} size="large" />
-        <Typography.Title level={3} className="logo"><Link to="/">Crypto-graphy</Link></Typography.Title>
-        <Button className="menu-control-container" ref={ref} onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
+        <Typography.Title level={3} className="logo">
+          <Link to="/">Crypto-graphy</Link>
+        </Typography.Title>
+        <Button
+          className="menu-control-container"
+          ref={ref}
+          onClick={() => setActiveMenu(!activeMenu)}
+        >
+          <MenuOutlined />
+        </Button>
       </div>
-             
-        {activeMenu ? ( <Menu theme="dark">
-                    <Menu.Item icon={<HomeOutlined/>}>
-                        <Link to='/'>home</Link>
-                    </Menu.Item> 
-                    <Menu.Item icon={<FundOutlined/>}>
-                        <Link to='/cryptocurrencies'>cryptocurrencies</Link>
-                    </Menu.Item>
-                    <Menu.Item icon={<MoneyCollectOutlined/>}>
-                        <Link to='/exchanges'>Exchanges</Link>
-                    </Menu.Item>
-                    <Menu.Item icon={<BulbOutlined/>}>
-                        <Link to='/news'>News</Link>
-                    </Menu.Item>
-                </Menu>) : "" }
-        </div>
-    )
-}
 
+      {activeMenu ? (
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
